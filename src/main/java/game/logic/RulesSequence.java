ba9cd46152f3999bcfe5d8dde90f9a3903e0e605
Ruslanci <66824783+Ruslanci@users.noreply.github.com>
2023-10-11 11:27:52 +0500

    #3 Refactored methods names in RulesLogic class to camelCase.

--- src/main/java/game/logic/RulesSequence.java
+++ src/main/java/game/logic/RulesSequence.java
@@ -3,10 +3,10 @@ package game.logic;
 //Class where are stored all the rules
 public class RulesSequence {
     public final static Rule[] sequence = new Rule[] {
-            new Rule(RulesLogic::Rule0_isNotEmpty), //<-- control rule: password can't be empty
-            new Rule(RulesLogic::Rule1_fiveCharacters),
-            new Rule(RulesLogic::Rule2_includesDigits),
-            new Rule(RulesLogic::Rule3_includesUpperCase),
-            new Rule(RulesLogic::Rule4_includesSpecial)
+            new Rule(RulesLogic::rule0_isNotEmpty), //<-- control rule: password can't be empty
+            new Rule(RulesLogic::rule1_fiveCharacters),
+            new Rule(RulesLogic::rule2_includesDigits),
+            new Rule(RulesLogic::rule3_includesUpperCase),
+            new Rule(RulesLogic::rule4_includesSpecial)
     };
 }
