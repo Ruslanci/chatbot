07dd3347091b78fb4eb0b6d7f72e82b2373e2169
ShudderStorm <shudderstorm@yandex.ru>
2023-11-11 22:07:33 +0500

    Исправлены некорректные тесты

--- src/test/java/game/logic/PasswordProcessorTest.java
+++ src/test/java/game/logic/PasswordProcessorTest.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class PasswordProcessorTest {
 
     @Test
-    public void testProcessAndGetStatus_AllRulesSatisfied() {
+    public void testProcessAndGetStatusAllRulesSatisfied() {
         PasswordProcessor passwordProcessor = new PasswordProcessor();
-        List<String> status = passwordProcessor.ProcessAndGetStatus("GoodPassword##123");
+        List<String> status = passwordProcessor.processAndGetStatus("GoodPassword##123");
 
         assertEquals(4, status.size());
         assertEquals("+ Your password must be at least 5 characters", status.get(0));
@@ -19,32 +19,18 @@ public class PasswordProcessorTest {
     }
 
     @Test
-    public void testProcessAndGetStatus_NotAllRulesSatisfied() {
+    public void testRulesSatisfiedAllRulesSatisfied() {
         PasswordProcessor passwordProcessor = new PasswordProcessor();
-        List<String> status = passwordProcessor.ProcessAndGetStatus("WeakPassword");
+        passwordProcessor.processAndGetStatus("GoodPas(#33sword123");
 
-        assertEquals(4, status.size()); // Assuming there are three rules in your RulesSequence
-        assertEquals("+ Your password must be at least 5 characters", status.get(0));
-        assertEquals("- Your password must include a number", status.get(1));
-        assertEquals("+ Your password must include an uppercase letter", status.get(2));
-        assertEquals("- Your password must include a special character", status.get(3));
-
-
+        assertTrue(passwordProcessor.isFinished());
     }
 
     @Test
-    public void testRulesSatisfied_AllRulesSatisfied() {
+    public void testRulesSatisfiedNotAllRulesSatisfied() {
         PasswordProcessor passwordProcessor = new PasswordProcessor();
-        passwordProcessor.ProcessAndGetStatus("GoodPas(#33sword123");
+        passwordProcessor.processAndGetStatus("WeakPassword");
 
-        assertTrue(passwordProcessor.RulesSatisfied());
-    }
-
-    @Test
-    public void testRulesSatisfied_NotAllRulesSatisfied() {
-        PasswordProcessor passwordProcessor = new PasswordProcessor();
-        passwordProcessor.ProcessAndGetStatus("WeakPassword");
-
-        assertFalse(passwordProcessor.RulesSatisfied());
+        assertFalse(passwordProcessor.isFinished());
     }
 }
