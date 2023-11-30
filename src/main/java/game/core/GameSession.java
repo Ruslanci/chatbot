cd9dc119d133d52eadff753339101a4943be35e4
Ruslanci <66824783+Ruslanci@users.noreply.github.com>
2023-11-30 23:04:14 +0500

    Избавились от потоков.
    Реализовано исполнение сессии путем создания хешмапы в ChatBot.

--- src/main/java/game/core/GameSession.java
+++ src/main/java/game/core/GameSession.java
@@ -1,57 +1,39 @@
 package game.core;
 import game.logic.PasswordProcessor;
 import game.telegram.ChatBot;
-import game.util.MessageTrader;
 
 import java.util.LinkedList;
-import java.util.concurrent.CompletableFuture;
 
 public class GameSession {
     private final PasswordProcessor processor;
-    private final MessageTrader channel;
     private final Long chatId;
+    private final Long userId;
+
     private final ChatBot chatBot;
-    private boolean running;
-    public GameSession(ChatBot chatBot, Long chatId) {
+    public GameSession(ChatBot chatBot, Long chatId, Long userId) {
         this.processor = new PasswordProcessor();
-        this.channel = new MessageTrader();
         this.chatId = chatId;
+        this.userId = userId;
         this.chatBot = chatBot;
-        this.running = false;
     }
     public void start() {
-        running = true;
-        runSession();
-    }
-    public void stop() {
-        running = false;
+        chatBot.sendText("Enter a password: ", chatId);
     }
 
-    public boolean isRunning() {
-        return running;
-    }
     public void putMessage(String message) {
-        channel.put(message);
+        String userEnteredPassword = message;
+
+        if (userEnteredPassword != null) {
+            this.checkPassword(userEnteredPassword).forEach(msg -> chatBot.sendText(msg, chatId));
+        }
+        if (processor.isFinished()) {
+            chatBot.sendText("Congratulations!", chatId);
+            chatBot.endGame(userId, chatId);
+        }
+
     }
     public LinkedList<String> checkPassword(String password) {
         return processor.processAndGetStatus(password);
     }
 
-    public void runSession() {
-        CompletableFuture.runAsync(() -> {
-            while (!processor.isFinished() && running) {
-                chatBot.sendText("Enter your password:", chatId);
-                String userEnteredPassword = channel.get();
-
-                if (userEnteredPassword != null) {
-                    this.checkPassword(userEnteredPassword).forEach(msg -> chatBot.sendText(msg, chatId));
-                }
-            }
-
-            if (running) {
-                chatBot.sendText("Congratulations! Type /start to begin a new game.", chatId);
-                running = false;
-            }
-        });
-    }
 }
