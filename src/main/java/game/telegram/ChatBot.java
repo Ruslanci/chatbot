78a4e8c7cbffadfeb1dec0e225c7514a5e3a2df2
Ruslanci <66824783+Ruslanci@users.noreply.github.com>
2023-11-30 21:41:51 +0500

    Избавились от потоков
    Вместо потоков теперь используется хешмапа, в которой хранятся id пользователей и их GameSession, соотвественно были переработаны методы readText(), startGame(), endGame()

--- src/main/java/game/telegram/ChatBot.java
+++ src/main/java/game/telegram/ChatBot.java
@@ -1,7 +1,6 @@
 package game.telegram;
 
 import game.core.GameSession;
-import game.core.SessionThread;
 import org.telegram.abilitybots.api.bot.AbilityBot;
 import org.telegram.abilitybots.api.objects.Ability;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -11,14 +10,17 @@ import static org.telegram.abilitybots.api.objects.Flag.TEXT;
 import static org.telegram.abilitybots.api.objects.Locality.ALL;
 import static org.telegram.abilitybots.api.objects.Privacy.PUBLIC;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChatBot extends AbilityBot {
-    private static final String tokenPath = "PasswordGameBotToken";
-    private static final String namePath = "PasswordGameBotName";
-    private SessionThread sessionThread;
+    private static final String tokenPath = "6114667694:AAGSk7z5Ww14pCl2JNcvs8h3gWOXW3mlJbY";
+    private static final String namePath = "Password_Games_bot";
+    private final Map<Long, GameSession> userSessions;
 
     public ChatBot() {
-        super(System.getenv(tokenPath), System.getenv(namePath));
-        sessionThread = null;
+        super(tokenPath, namePath);
+        userSessions = new HashMap<>();
     }
 
     @Override
@@ -37,6 +39,10 @@ public class ChatBot extends AbilityBot {
         }
     }
 
+    private GameSession getSession(Long userId, Long chatId) {
+        return userSessions.computeIfAbsent(userId, key -> new GameSession(this, chatId));
+    }
+
     public Ability readText() {
         return Ability.builder()
                 .name(DEFAULT)
@@ -44,21 +50,41 @@ public class ChatBot extends AbilityBot {
                 .locality(ALL)
                 .privacy(PUBLIC)
                 .action(ctx -> {
-                    if (sessionThread == null || !sessionThread.isAlive())
-                        sendText("You are not in session, type /start to play", ctx.chatId());
-                    else
-                        sessionThread.putMessage(ctx.update().getMessage().getText());
+                    Long userId = ctx.user().getId();
+                    GameSession session = userSessions.get(userId);
+
+                    if (session == null || !session.isRunning()) {
+                        sendText("You are not in a session, type /start to play", ctx.chatId());
+                    } else {
+                        session.putMessage(ctx.update().getMessage().getText());
+                    }
                 })
                 .build();
     }
+
+    
+
     public Ability startGame() {
         return Ability.builder()
                 .name("start")
                 .locality(ALL)
                 .privacy(PUBLIC)
                 .action(ctx -> {
-                    sessionThread = new SessionThread(new GameSession(this, ctx.chatId()));
-                    sessionThread.start();
+                    Long userId = ctx.user().getId();
+                    Long chatId = ctx.chatId();
+
+                    GameSession existingSession = userSessions.remove(userId);
+                    if (existingSession != null) {
+                        existingSession.stop();
+                    }
+
+                    GameSession session = getSession(userId, chatId);
+
+                    if (session.isRunning()) {
+                        sendText("You are already in a session.", chatId);
+                    } else {
+                        session.start();
+                    }
                 })
                 .build();
     }
@@ -69,8 +95,16 @@ public class ChatBot extends AbilityBot {
                 .locality(ALL)
                 .privacy(PUBLIC)
                 .action(ctx -> {
-                    sessionThread.interrupt();
-                    sendText("Your are not in session anymore, type /start to play again", ctx.chatId());
+                    Long userId = ctx.user().getId();
+                    Long chatId = ctx.chatId();
+                    
+                    GameSession session = userSessions.remove(userId);
+                    if (session != null) {
+                        session.stop();
+                        sendText("Game session ended. Type /start to play again.", chatId);
+                    } else {
+                        sendText("You are not in a session.", chatId);
+                    }
                 })
                 .build();
     }
