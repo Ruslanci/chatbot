517485023743eb2426edaf043ea609fca804de6e
Ruslanci <66824783+Ruslanci@users.noreply.github.com>
2023-11-30 21:42:19 +0500

    Create RulesTest.java

---
+++ src/test/java/game/logic/RulesTest.java
@@ -0,0 +1,47 @@
+package game.logic;
+import game.logic.rules.DigitsIncludingRule;
+import game.logic.rules.FiveCharactersRule;
+import game.logic.rules.SpecialIncludingRule;
+import game.logic.rules.UpperCaseIncludingRule;
+import org.junit.Test;
+import org.junit.Assert;
+
+
+public class RulesTest {
+    Rule rule1 = new FiveCharactersRule();
+    Rule rule2 = new DigitsIncludingRule();
+    Rule rule3 = new UpperCaseIncludingRule();
+    Rule rule4 = new SpecialIncludingRule();
+    @Test
+    public void fiveCharactersRuleTestTrue() {
+        Assert.assertTrue(rule1.match("qwert"));
+    }
+    @Test
+    public void fiveCharactersRuleTestFalse() {
+        Assert.assertFalse(rule1.match("qwer"));
+    }
+    @Test
+    public void digitsIncludingRuleTestTrue() {
+        Assert.assertTrue(rule2.match("123"));
+    }
+    @Test
+    public void digitsIncludingRuleTestFalse() {
+        Assert.assertFalse(rule2.match("abc"));
+    }
+    @Test
+    public void upperCaseIncludingRuleTestTrue() {
+        Assert.assertTrue(rule3.match("ABC"));
+    }
+    @Test
+    public void upperCaseIncludingRuleTestFalse() {
+        Assert.assertFalse(rule3.match("abc"));
+    }
+    @Test
+    public void specialIncludingTestTrue() {
+        Assert.assertTrue(rule4.match("!@#$"));
+    }
+    @Test
+    public void specialIncludingTestFalse() {
+        Assert.assertFalse(rule4.match("1234"));
+    }
+}
