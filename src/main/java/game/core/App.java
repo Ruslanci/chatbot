fc5b9bf2c08caa04c678f071ffac107c49a8d3e5
Ruslanci <66824783+Ruslanci@users.noreply.github.com>
2023-12-01 21:04:16 +0500

    Formatted

--- src/main/java/game/core/App.java
+++ src/main/java/game/core/App.java
@@ -6,12 +6,13 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class App {
-    public static void main(String[] args) {
-        try {
-            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new ChatBot());
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
+
+  public static void main(String[] args) {
+    try {
+      TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+      botsApi.registerBot(new ChatBot());
+    } catch (TelegramApiException e) {
+      e.printStackTrace();
     }
+  }
 }
