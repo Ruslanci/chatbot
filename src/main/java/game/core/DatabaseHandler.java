ec17a6d11e300b54711b14a85a5a287015863f67
Ruslanci <66824783+Ruslanci@users.noreply.github.com>
2023-12-20 23:18:23 +0500

    Added a new column won_duels which allows to store the number of duels won by the person

--- src/main/java/game/core/DatabaseHandler.java
+++ src/main/java/game/core/DatabaseHandler.java
@@ -28,7 +28,8 @@ public class DatabaseHandler {
                 + "(id INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + "user_id INTEGER NOT NULL, "
                 + "username TEXT NOT NULL, "
-                + "duration INTEGER NOT NULL)"
+                + "duration INTEGER NOT NULL,"
+                + "won_duels INTEGER NOT NULL DEFAULT 0)"
         );
       }
 
@@ -44,10 +45,10 @@ public class DatabaseHandler {
     List<String> leaderboard = new ArrayList<>();
 
     try (PreparedStatement preparedStatement = connection.prepareStatement(
-        "SELECT user_id, username, MIN(duration) AS fastest_duration " +
+        "SELECT user_id, username, MIN(duration) AS fastest_duration, won_duels " +
             "FROM game_sessions " +
             "WHERE duration IS NOT NULL " +
-            "GROUP BY user_id, username " +
+            "GROUP BY user_id, username, won_duels " +
             "ORDER BY fastest_duration ASC " +
             "LIMIT 5"
     )) {
@@ -56,8 +57,10 @@ public class DatabaseHandler {
       while (resultSet.next()) {
         String username = resultSet.getString("username");
         long fastestDuration = resultSet.getLong("fastest_duration");
+        int wonDuels = resultSet.getInt("won_duels");
         leaderboard.add(
-            "Username: " + username + ", Fastest Duration: " + fastestDuration / 1000.0f + " seconds");
+            "Username: " + username + ", Fastest Duration: " + fastestDuration / 1000.0f + " seconds"
+                + ", Won Duels: " + wonDuels);
       }
     } catch (SQLException e) {
       e.printStackTrace();
@@ -65,15 +68,38 @@ public class DatabaseHandler {
 
     return leaderboard;
   }
+  public List<String> getBestDuelists() {
+    List<String> bestDuelists = new ArrayList<>();
+
+    try (PreparedStatement preparedStatement = connection.prepareStatement(
+        "SELECT user_id, username, won_duels " +
+            "FROM game_sessions " +
+            "WHERE won_duels > 0 " +
+            "ORDER BY won_duels DESC " +
+            "LIMIT 5"
+    )) {
+      ResultSet resultSet = preparedStatement.executeQuery();
+
+      while (resultSet.next()) {
+        String username = resultSet.getString("username");
+        int wonDuels = resultSet.getInt("won_duels");
+        bestDuelists.add("Username: " + username + ", Won Duels: " + wonDuels);
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return bestDuelists;
+  }
   /** Saves a game session to the 'game_sessions' table.
    * @param userId   The user ID associated with the session.
    * @param username The username associated with the session.
    * @param duration The duration of the session. */
-  public void saveGameSessionToDatabase(Long userId, String username, long duration) {
+  public void saveGameSessionToDatabase(Long userId, String username, long duration, boolean wonDuel) {
     try {
       // Check if the user already exists in the database
       try (PreparedStatement checkStatement = connection.prepareStatement(
-          "SELECT duration FROM game_sessions WHERE user_id = ?"
+          "SELECT duration, won_duels FROM game_sessions WHERE user_id = ?"
       )) {
         checkStatement.setLong(1, userId);
         ResultSet resultSet = checkStatement.executeQuery();
@@ -81,20 +107,22 @@ public class DatabaseHandler {
         if (resultSet.next()) {
           // User exists, check if the new duration is smaller
           long existingDuration = resultSet.getLong("duration");
+          int existingWonDuels = resultSet.getInt("won_duels");
           if (duration < existingDuration) {
             // Update the existing entry with the new duration
             try (PreparedStatement updateStatement = connection.prepareStatement(
-                "UPDATE game_sessions SET duration = ? WHERE user_id = ?"
+                "UPDATE game_sessions SET duration = ?, won_duels = ? WHERE user_id = ?"
             )) {
               updateStatement.setLong(1, duration);
-              updateStatement.setLong(2, userId);
+              updateStatement.setLong(2, wonDuel ? existingWonDuels + 1 : existingWonDuels);
+              updateStatement.setLong(3, userId);
               updateStatement.executeUpdate();
             }
           }
         } else {
           // User does not exist, insert a new entry
           try (PreparedStatement insertStatement = connection.prepareStatement(
-              "INSERT INTO game_sessions (user_id, username, duration) VALUES (?, ?, ?)"
+              "INSERT INTO game_sessions (user_id, username, duration, won_duels) VALUES (?, ?, ?, 0)"
           )) {
             insertStatement.setLong(1, userId);
             insertStatement.setString(2, username);
